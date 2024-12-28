0dc2b393722ae60f342853bf3dfdcc81dd80d3de
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2024-12-28 10:52:56 -0300

    v.1.13

--- mergePDFs.py
+++ mergePDFs.py
@@ -1,14 +1,17 @@
 '''
-Merge PDFs 
+Merge PDFs Project- By Paulo Fiuza- 12/28/2024
 '''
 import PyPDF2 as PDF
 import os
 
+'''Create the merger'''
 merger = PDF.PdfMerger()
 print(f"Merger = {merger}")
 
+'''Create the list of archives- project Folder '''
 archivesList = os.listdir()
 print(f"Archives List= {archivesList}")
 
+'''Create the list of archives- Folder archives'''
 archivesList = os.listdir("archives")
-print(f"Archives List= {archivesList}") 
+print(f"Archives List= {archivesList}")
